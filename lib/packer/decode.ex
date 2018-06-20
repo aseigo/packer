737ed66e7bddcf0800e6a79a43e84b7ddf573e50
Aaron Seigo <aseigo@exote.ch>
2018-06-20 13:24:11 +0200

    support compressed schemas in lists

--- lib/packer/decode.ex
+++ lib/packer/decode.ex
@@ -38,6 +38,10 @@ defmodule Packer.Decode do
     decode_next_list_item(rem_schema, buffer, opts, [])
   end
 
+  defp decode_one(<<type :: 8-unsigned-integer, rem_schema :: binary>>, buffer, opts) do
+    debuffer_one(type, rem_schema, buffer, opts)
+  end
+
   decode_primitive(@c_small_int, 1, 8-signed-integer, 0)
   decode_primitive(@c_small_uint, 1, 8-unsigned-integer, 0)
   decode_primitive(@c_short_int, 2, 16-signed-integer, 0)
@@ -63,8 +67,27 @@ defmodule Packer.Decode do
     decoded(rem_schema, buffer, opts, Enum.reverse(acc))
   end
 
+  defp decode_next_list_item(<<@c_repeat_1, rem_schema :: binary>>, buffer, opts, acc) do
+    if byte_size(buffer) < 1 do
+      #TODO is it really right to just ignore it at this point?
+      decoded(rem_schema, buffer, opts, acc)
+    else
+      <<count :: 8-unsigned-integer, type :: 8-unsigned-integer, rem_schema :: binary>> = rem_schema
+      decode_n_list_items(type, rem_schema, buffer, opts, acc, count)
+    end
+  end
+
   defp decode_next_list_item(schema, buffer, opts, acc) do
     {rem_schema, rem_buffer, term} = decode_one(schema, buffer, opts)
     decode_next_list_item(rem_schema, rem_buffer, opts, [term | acc])
+  end
+
+  defp decode_n_list_items(_type, schema, buffer, opts, acc, 0) do
+    decode_next_list_item(schema, buffer, opts, acc)
+  end
+
+  defp decode_n_list_items(type, schema, buffer, opts, acc, count) do
+    {rem_schema, rem_buffer, term} = debuffer_one(type, schema, buffer, opts)
+    decode_n_list_items(type, rem_schema, rem_buffer, opts, [term | acc], count - 1)
   end
 end
