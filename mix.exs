1ee982998cfe0996b05633540f986587b3898f80
Aaron Seigo <aseigo@exote.ch>
2018-06-24 21:21:10 +0200

    bump the version #

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pack.MixProject do
   def project do
     [
       app: :packer,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
